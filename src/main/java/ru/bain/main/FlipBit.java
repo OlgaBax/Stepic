50298c55418e3479f1e2406a86456546a933cd50
Babushkina Olga <ba80@mail.ru>
2022-03-14 17:11:40 +0300

    Изменила FlipBit

--- src/main/java/ru/bain/main/FlipBit.java
+++ src/main/java/ru/bain/main/FlipBit.java
@@ -2,11 +2,12 @@ package ru.bain.main;
 
 public class FlipBit {
     public static void main(String[] args) {
-        flipBit(15,1);
+        flipBit(15, 1);
+        System.out.println();
 
     }
 
-    public static int flipBit(int value, int bitIndex){
-       return value^(1<<(bitIndex-1));
+    public static int flipBit(int value, int bitIndex) {
+        return value ^ (1 << (bitIndex - 1));
     }
 }
